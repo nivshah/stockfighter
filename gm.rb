654ae49e82e6f065c23865f1419e821067741844
Niv Shah <nivshah@gmail.com>
2015-12-28 14:28:09 -0500

    Sweeeeeeeeeeeeet

---
+++ gm.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'httparty'
+require 'json'
+
+module StockFighter
+    class GameManager
+        attr_accessor :api_key, :level_name, :instance_id, :account, :venue, :ticker
+        BaseUrl = "https://www.stockfighter.io/gm"
+        def initialize(api_key, level_name)
+            self.api_key = api_key
+            self.level_name = level_name
+
+            self.start
+        end
+
+        def level_url
+            "#{BaseUrl}/levels/#{self.level_name}"
+        end
+
+        def start
+            response = HTTParty.post(self.level_url,
+                          :cookies => {"api_key" => self.api_key}
+                         )
+            self.instance_id = response.parsed_response['instanceId']
+            self.account = response.parsed_response['account']
+            self.venue = response.parsed_response['venues'][0]
+            self.ticker = response.parsed_response['tickers'][0]
+        end
+
+        def restart
+        end
+
+        def stop
+        end
+
+        def resume
+        end
+
+        def state
+            response = HTTParty.get(self.level_url,
+                                    :cookies => {"api_key" => self.api_key}
+                                   )
+
+            return response.body
+        end
+    end
+end
